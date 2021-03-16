4c67516850cac1ad2dd227164c0806ebbdef498d
Brandon James <somerandomguy1964@gmail.com>
2021-03-16 10:53:20 -0500

    Add files via upload
    hello.c from Linux Directory

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>   
+int main()
+{
+	printf("Hello World\n");   // hello Brandon
+	functionCall();
+ 	exit(0);  
+}       // end of the program
+functionCall()
+{
+	printf("this is a function call\n");
+}
